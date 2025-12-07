f015add425d721ca2a5b76e5cc2e684eb2c33812
Anton Samoshin <asamoshin9@gmail.com>
2025-12-07 18:09:41 +0300

    Add basic repositories tests

--- test/controllers/web/repositories_controller_test.rb
+++ test/controllers/web/repositories_controller_test.rb
@@ -3,8 +3,21 @@
 require 'test_helper'
 
 class Web::RepositoriesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:developer)
+
+    @repository = repositories(:ruby_repo)
+  end
+
   test 'should get index' do
-    get web_repositories_index_url
+    get repositories_path
+    assert_response :success
+  end
+
+  test 'should get new' do
+    sign_in(@user)
+
+    get new_repository_path
     assert_response :success
   end
 end
