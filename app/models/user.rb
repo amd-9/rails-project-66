c229ff58a501f08465e0c7a8b7b37b16f1fb34bd
Anton Samoshin <asamoshin9@gmail.com>
2026-02-08 12:00:22 +0300

    Add enumerize support for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  extend Enumerize
+
+  enumerize :language, in: %i[ruby js], default: :ruby
+
   has_many :repositories, dependent: :destroy
 end
