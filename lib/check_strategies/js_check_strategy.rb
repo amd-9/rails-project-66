d7a0231ca9978776c6724bed2c85fb25d5dda255
Anton Samoshin <asamoshin9@gmail.com>
2026-08-23 22:15:35 +0300

    Refactor JS check strategy

--- lib/check_strategies/js_check_strategy.rb
+++ lib/check_strategies/js_check_strategy.rb
@@ -4,6 +4,6 @@ class JSCheckStrategy
   def check(container, command_executor)
     command_executor.container_exec_command(container, %w[npm install --save-dev eslint @eslint/js])
     command_executor.container_exec_command(container, %w[npx eslint -v])
-    command_executor.container_exec_command(container, %w[npx eslint -f json .])
+    command_executor.container_exec_command(container, %w[npx eslint -f json --config eslint.config.mjs --no-config-lookup .])
   end
 end
