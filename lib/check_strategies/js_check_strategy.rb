8bbbcaa475e7bd24d7e222f8e28236cd4be14bed
Anton Samoshin <asamoshin9@gmail.com>
2026-08-24 18:59:01 +0300

    Refactor JS check strategy

--- lib/check_strategies/js_check_strategy.rb
+++ lib/check_strategies/js_check_strategy.rb
@@ -2,8 +2,10 @@
 
 class JSCheckStrategy
   def check(container, command_executor)
-    command_executor.container_exec_command(container, %w[npm install --save-dev eslint @eslint/js])
-    command_executor.container_exec_command(container, %w[npx eslint -v])
-    command_executor.container_exec_command(container, %w[npx eslint -f json --config eslint.config.mjs --no-config-lookup .])
+    command_executor.container_exec_command(container, ['mkdir', '/eslint-runner'])
+    command_executor.container_exec_command(container, ['bash', '-c', 'cd /eslint-runner && npm install eslint@10.9.0 @eslint/js --save-dev'])
+    command_executor.container_exec_command(container, ['bash', '-c', 'cd /eslint-runner && npx eslint -v'])
+    command_executor.container_exec_command(container, ['cp', 'eslint.config.mjs', '/eslint-runner/eslint.config.mjs'])
+    command_executor.container_exec_command(container, ['bash', '-c', 'NODE_OPTIONS="--no-warnings" /eslint-runner/node_modules/.bin/eslint -f json --config /eslint-runner/eslint.config.mjs --no-config-lookup .'])
   end
 end
